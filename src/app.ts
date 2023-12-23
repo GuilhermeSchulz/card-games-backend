fd6c6941e3d46725bc698d7d643d2d9b337089cc
Anderson Keller <andersonkellervendas@hotmail.com>
2023-12-23 20:17:23 -0300

    socket

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,33 @@
-import "express-async-errors";
-import "reflect-metadata";
-import express, { Request, Response } from "express";
 import cors from "cors";
-import { userRouter } from "./routes/User.routes";
+import express from "express";
+import "express-async-errors";
 import { createServer } from "http";
+import "reflect-metadata";
 import { Server } from "socket.io";
+import { userRouter } from "./routes/User.routes";
 const corsOptions = {
-    origin: '*'
-  }
-
+  origin: "*",
+};
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use('/user', userRouter)
+app.use("/user", userRouter);
 const http = createServer();
-const io = new Server(http, {cors: corsOptions});
+const io = new Server(http, { cors: corsOptions });
 
-io.on("connection", function(socket) {
-    console.log("socket connected");
-    io.emit("user connected");
+io.on("connection", function (socket) {
+  console.log("socket connected");
+  const rooms = ["room1", "room2"];
+  socket.on("create-match", (arg, callback) => {
+    console.log(arg);
+    callback(rooms);
+    socket.emit("judas", time, (data: any) => {
+      console.log(data);
+      return "judas";
+    });
+  });
+  let time = 30;
 });
 
-export {app, http};
+export { app, http };
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
-import {app, http} from "./app";
-import {AppDataSource} from "./data-source";
+import { app, http } from "./app";
+import { AppDataSource } from "./data-source";
 
 (async () => {
+  await AppDataSource.initialize().catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
 
-    await AppDataSource.initialize()
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
-    
-    app.listen(process.env.PORT, () => {
-        console.log("Servidor executando")
-    })  
-    http.listen(3000, () => console.log("listening on http://localhost:3000")) 
-})()
+  app.listen(process.env.PORT, () => {
+    console.log("Servidor executando");
+  });
+  http.listen(3001, () => console.log("listening on http://localhost:3001"));
+})();
