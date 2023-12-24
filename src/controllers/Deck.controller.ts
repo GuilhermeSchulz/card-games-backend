8e5751ae88c6227a05ea826d151ef69173fba6af
Guilherme Schulz <gui.schulz@gmail.com>
2023-12-23 23:55:37 -0300

    fix: fixed deck controller to return the object

--- src/controllers/Deck.controller.ts
+++ src/controllers/Deck.controller.ts
@@ -6,6 +6,6 @@ import { createDeck } from "../service/Deck.service";
 export async function createDeckController(req: Request, res: Response) {
     const deckData = req.body;
     const id = req.user.id
-    const deck = createDeck(id, deckData)
+    const deck =await createDeck(id, deckData)
     return res.status(201).json(deck)
 }
