0eb6a9a9fc2a2e62b5ca6cef48e9ed4c5afa44f4
AndersonKeller <andersonkellervendas@hotmail.com>
2023-12-26 22:03:26 -0300

    fix: routes to cards and decks

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,15 @@ import "express-async-errors";
 
 import "reflect-metadata";
 
+import { cardRouter } from "./routes/Card.routes";
+import { deckRouter } from "./routes/Deck.routes";
 import { userRouter } from "./routes/User.routes";
 
-import { io } from "./socket";
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/user", userRouter);
+app.use("/cards", cardRouter);
+app.use("/deck", deckRouter);
 
 export { app };
